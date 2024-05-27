1b1f29981a554fb207ee78d87bf7450f12342411
xiaoh105 <huruiyan105@126.com>
2024-05-28 00:30:57 +0800

    Upd: Adjusted parameters. Add compile flags.

--- src/executor/executor.cpp
+++ src/executor/executor.cpp
@@ -31,13 +31,13 @@ shared_ptr<UserSystem> user_system;
 shared_ptr<TrainSystem> ticket_system;
 
 void Initialize() {
-  const auto user_buffer = new BufferPoolManager(150, make_unique<DiskManager>("user.dat"));
+  const auto user_buffer = new BufferPoolManager(100, make_unique<DiskManager>("user.dat"));
   user_system = make_shared<UserSystem>(shared_ptr(user_buffer));
-  const auto train_buffer = new BufferPoolManager(350, make_unique<DiskManager>("train.dat"));
-  const auto station_buffer = new BufferPoolManager(180, make_unique<DiskManager>("station.dat"));
-  const auto waitlist_buffer = new BufferPoolManager(150, make_unique<DiskManager>("waitlist.dat"));
-  const auto orderlist_buffer = new BufferPoolManager(150, make_unique<DiskManager>("orderlist.dat"));
-  const auto ticket_buffer = new BufferPoolManager(180, make_unique<DiskManager>("ticket.dat"));
+  const auto train_buffer = new BufferPoolManager(300, make_unique<DiskManager>("train.dat"));
+  const auto station_buffer = new BufferPoolManager(100, make_unique<DiskManager>("station.dat"));
+  const auto waitlist_buffer = new BufferPoolManager(100, make_unique<DiskManager>("waitlist.dat"));
+  const auto orderlist_buffer = new BufferPoolManager(100, make_unique<DiskManager>("orderlist.dat"));
+  const auto ticket_buffer = new BufferPoolManager(100, make_unique<DiskManager>("ticket.dat"));
   ticket_system = make_shared<TrainSystem>(shared_ptr(train_buffer), shared_ptr(station_buffer),
                                            shared_ptr(ticket_buffer), shared_ptr(waitlist_buffer),
                                            shared_ptr(orderlist_buffer));
